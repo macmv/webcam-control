264622f872a8fc37bb36b81fc8c20f5ea62aac5f
Neil Macneale <neil.macneale.v@gmail.com>
2020-09-03 11:03:01 -0700

    Added go.mod, and updated imports to be the correct paths

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/macmv/webcam-control
+
+go 1.15
+
+require (
+	github.com/gizak/termui v3.1.0+incompatible
+	github.com/gizak/termui/v3 v3.1.0
+)
--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
   "strings"
   "strconv"
 
-  ui "github.com/gizak/termui"
-  "github.com/gizak/termui/widgets"
+  ui "github.com/gizak/termui/v3"
+  "github.com/gizak/termui/v3/widgets"
 )
 
 func main() {
